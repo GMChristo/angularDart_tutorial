54f05ae9aa03f8e6f476c3ea493aa56b1a9a61d3
GMChristo <gabrielchristo@hotmail.com>
2021-09-02 13:42:51 -0300

    att pagina de cursos e criado model do mesmo

--- lib/src/components/courses/courses_component.dart
+++ lib/src/components/courses/courses_component.dart
@@ -1,10 +1,41 @@
 import 'package:angular/angular.dart';
+import 'package:angularDart_tutorial/src/model/course_model.dart';
 
 @Component(
   selector: 'courses',
   templateUrl: 'courses_template.html',
+  directives: [coreDirectives],
   providers: [],
 )
 class CoursesComponent {
-  
+  List<Course> courses = [
+    Course(
+      '111',
+      'Javascript: the complete course',
+      'CodeReview',
+      19.99,
+      'Full understanding of Javascript ecosystem',
+      10,
+      'assets/images/1.png',
+      ['Lecture1', 'Lecture2'],
+      ['Javascript', 'programming'],
+      DateTime.now().toIso8601String(),
+      ),
+    Course(
+      '112',
+      'Javascript: the complete course',
+      'CodeReview',
+      19.99,
+      'Full understanding of Javascript ecosystem',
+      10,
+      'assets/images/2.png',
+      ['Lecture1', 'Lecture2'],
+      ['Javascript', 'programming'],
+      DateTime.now().toIso8601String(),
+      ),
+  ];
+
+  void deleteItem(String uid) {
+    courses.removeWhere((course) => course.uid == uid);
+  }
 }
